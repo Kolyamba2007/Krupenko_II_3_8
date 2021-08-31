f5fdb997421dbc0f8168beaa961965f2a762b323
Kolyamba2007 <55401238+Kolyamba2007@users.noreply.github.com>
2021-08-31 17:25:21 +0300

    Commit5

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ziggurat.Units;
+
+public class GameManager : MonoBehaviour
+{
+    LinkedList<IUnit> units = new LinkedList<IUnit>();
+
+    void AddUnit(IUnit unit)
+    {
+        if(units.Contains(unit))
+            units.AddLast(unit);
+    }
+
+    void RemoveUnit(IUnit unit)
+    {
+        units.Remove(unit);
+    }
+
+    IUnit ClosestEnemy(IUnit unit)
+    {
+        IUnit enemy = null;
+        float minDist = float.MaxValue;
+
+        foreach(IUnit e in units)
+        {
+            float dist = Vector3.Distance(unit.Position, e.Position);
+
+            if (unit != e && dist < minDist)
+            {
+                minDist = dist;
+                enemy = e;
+            }
+        }
+
+        return enemy;
+    }
+}
